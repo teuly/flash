6852ea4c9335a58cff0689060e9ad0fa52532fa7
teuly <diateuf@gmail.com>
2016-02-07 17:44:29 +0300

    Update card.rb

--- app/models/card.rb
+++ app/models/card.rb
@@ -5,7 +5,7 @@ validates :translated_text, presence: true
 validate :not_equal
 
 #VALID_REGEX = /\A[a-z]+\z/
-#vSalidates :original_text, :translated_text, format: { with:  VALID_REGEX } 
+#validates :original_text, :translated_text, format: { with:  VALID_REGEX } 
 
 def not_equal
 errors.add(:base, 'translated_text and original_text cannot be equal.') if translated_text == original_text
